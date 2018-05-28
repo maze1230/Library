a90c90cde2b4aeba7c902c4d9ec34f96dc525e02
maze1230 <maze1230@gmail.com>
2018-05-29 00:10:22 +0900

    wrap F f() by merge()

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -31,13 +31,14 @@ private:
 	T ti; // e0:F
 	vector<T> node;
 public:
+	SegTree(){}
 	SegTree(int32 _n, F f, G g, T ti):f(f), g(g), ti(ti){
 		init(_n);
 	}
 	SegTree(vector<T> v, F f, G g, T ti):f(f), g(g), ti(ti){
 		init(v.size());
 		for(int32 i = 0;i < v.size();i++) node[i+n-1] = v[i];
-		for(int32 i = n-2;i >= 0;i--) merge(i);
+		for(int32 i = n-2;i >= 0;i--) node[i] = merge(node[i*2+1], node[i*2+2]);
 	}
 
 	inline void init(int32 _n){
@@ -46,10 +47,10 @@ public:
 		node.resize(2*n-1, ti);
 	}
 
-	inline void merge(int32 k){
-		if(node[k*2+1] == ti) node[k] = node[k*2+2];
-		else if(node[k*2+2] == ti) node[k] = node[k*2+1];
-		else node[k] = f(node[k*2+1], node[k*2+2]);
+	inline T merge(T lhs, T rhs){
+		if(lhs == ti) return rhs;
+		else if(rhs == ti) return lhs;
+		return f(lhs, rhs);
 	}
 
 	void update(int32 k, E x){
@@ -57,7 +58,7 @@ public:
 		node[k] = g(node[k], x);
 		while(k){
 			k = (k-1)/2;
-			merge(k);
+			node[k] = merge(node[k*2+1], node[k*2+2]);
 		}
 	}
 
@@ -65,7 +66,7 @@ public:
 		if(r < 0) r = n;
 		if(b <= l || r <= a) return ti;
 		if(a <= l && r <= b) return node[k];
-		return f(query(a, b, k*2+1, l, (l+r)/2), query(a, b, k*2+2, (l+r)/2, r));
+		return merge(query(a, b, k*2+1, l, (l+r)/2), query(a, b, k*2+2, (l+r)/2, r));
 	}
 };
 
@@ -86,6 +87,7 @@ private:
 	T ti;
 	E ei;
 public:
+	LazySegTree(){}
 	LazySegTree(int32 _n, F f, G g, H h, T ti, E ei, P p = [](E a, int32 b){return a;}):f(f), g(g), h(h), p(p), ti(ti), ei(ei){
 		init(_n);
 	}
@@ -93,7 +95,7 @@ public:
 	LazySegTree(vector<T> v, F f, G g, H h, T ti, E ei, P p = [](E a, int32 b){return a;}):f(f), g(g), h(h), p(p), ti(ti), ei(ei){
 		init(v.size());
 		for(int32 i = 0;i < v.size();i++) node[i+n-1] = v[i];
-		for(int32 i = n-2;i >= 0;i--) merge(i);
+		for(int32 i = n-2;i >= 0;i--) node[i] = merge(node[i*2+1], node[i*2+2]);
 	}
 
 	void init(int32 _n){
@@ -103,10 +105,10 @@ public:
 		lazy.resize(2*n-1, ei);
 	}
 
-	inline void merge(int32 k){
-		if(node[k*2+1] == ti) node[k] = node[k*2+2];
-		else if(node[k*2+2] == ti) node[k] = node[k*2+1];
-		node[k] = f(node[k*2+2], node[k*2+1]);
+	inline T merge(T lhs, T rhs){
+		if(lhs == ti) return rhs;
+		else if(rhs == ti) return lhs;
+		return f(lhs, rhs);
 	}
 
 	inline void eval(int32 k, int32 l, int32 r){
@@ -127,7 +129,7 @@ public:
 			lazy[k] = h(lazy[k], x);
 			return g(node[k], p(lazy[k], r-l));
 	}
-		return node[k] = f(update(a, b, x, k*2+1, l, (l+r)/2), update(a, b, x, k*2+2, (l+r)/2, r));
+		return node[k] = merge(update(a, b, x, k*2+1, l, (l+r)/2), update(a, b, x, k*2+2, (l+r)/2, r));
 	}
 
 	T query(int32 a, int32 b, int32 k=0, int32 l=0, int32 r=-1){
@@ -135,7 +137,7 @@ public:
 		eval(k, l, r);
 		if(b <= l || r <= a) return ti;
 		if(a <= l && r <= b) return node[k];
-		return f(query(a, b, k*2+1, l, (l+r)/2), query(a, b, k*2+2, (l+r)/2, r));
+		return merge(query(a, b, k*2+1, l, (l+r)/2), query(a, b, k*2+2, (l+r)/2, r));
 	}
 };
 
