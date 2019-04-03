3115bb3d0d190f2698e92a9944938487621bb61c
maze1230 <maze1230@gmail.com>
2019-04-03 23:57:32 +0900

    add Binary Matrix

---
+++ Math/Garner.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+::std::int64_t gcd(::std::int64_t a, ::std::int64_t b) {
+	while (b != 0) {
+		::std::swap(a, b);
+		b = b % a;
+	}
+	return a;
+}
+
+::std::int64_t lcm(::std::int64_t a, ::std::int64_t b) {
+	return a / gcd(a, b) * b;
+}
+
+::std::pair<::std::int64_t, ::std::int64_t> extgcd(::std::int64_t a, ::std::int64_t b) {
+	::std::pair<::std::int64_t, ::std::int64_t> pa(1, 0), pb(0, 1);
+	while (b != 0) {
+		::std::swap(a, b); ::std::swap(pa, pb);
+		pb = ::std::make_pair(pb.first - pa.first * (b / a), pb.second - pa.second * (b / a));
+		b = b % a;
+	}
+	return pa;
+}
+
+
+// return x (mod lcm(m_i)) and lcm(m_i) that satisfies x ≡ b_i (mod m_i) (中国剰余定理)
+// if there isn't the answer, return (0, -1)
+// O(N log M)
+::std::pair<::std::int64_t, ::std::int64_t> CRT(const ::std::vector<::std::int64_t> &b, ::std::vector<::std::int64_t> &m) {
+	::std::int64_t ret = 0, M = 1;
+	for (::std::size_t i = 0; i < b.size(); ++i) {
+		::std::int64_t p, q;
+		::std::int64_t d = gcd(M, m[i]);
+		::std::tie(p, q) = extgcd(M, m[i]);
+		if ((b[i] - ret) % d != 0) return ::std::make_pair(0, -1);
+		::std::int64_t tmp = (b[i] - ret) / d * p % (m[i] / d);
+		ret += M * tmp;
+		M *= m[i] / d;
+	}
+	return ::std::make_pair((ret + M) % M, M);
+}
---
+++ Math/Matrix/BinaryMatrix.cpp
@@ -0,0 +1,109 @@
+#include <bits/std++.h>
+
+template<::std::size_t Column>
+class BinaryMatrix {
+public:
+	using size_type = ::std::size_t;
+	using value_type = ::std::uint64_t;
+	using Row = ::std::bitset<Column>;
+	using Mat = ::std::vector<Row>;
+
+private:
+	size_type R, C;
+	Mat A;
+
+	void add_row_to_another(size_type r1, size_type r2){ // Row(r1) += Row(r2)
+		A[r1] = A[r1] ^ A[r2];
+	}
+
+
+public:
+	BinaryMatrix() {}
+	BinaryMatrix(size_type R_, size_type C_) : R(R_), C(C_), A(Mat(R_)) {}
+	BinaryMatrix(const Mat& A_) : R(A_.size()), C(A_[0].size()), A(A_) {}
+	BinaryMatrix(const Mat&& A_) : R(A_.size()), C(A_[0].size()), A(A_) {}
+	BinaryMatrix(const BinaryMatrix& m) : R(m.R), C(m.C), A(m.A) {}
+	BinaryMatrix(const BinaryMatrix&& m) : R(m.R), C(m.C), A(m.A) {}
+
+	BinaryMatrix &operator=(const BinaryMatrix &m){
+ 		R = m.R; C = m.C; A = m.A;
+		return *this;
+	}
+	BinaryMatrix &operator=(const BinaryMatrix &&m){
+ 		R = m.R; C = m.C; A = m.A;
+		return *this;
+	}
+	static BinaryMatrix I(const size_type N){
+		BinaryMatrix m(N, N);
+		for(size_type i = 0;i < N;i++) m[i][i] = 1;
+		return m;
+	}
+
+	const Row& operator[](size_type k) const& { return A.at(k); }
+	Row& operator[](size_type k) & { return A.at(k); }
+	Row operator[](size_type k) const&& { return ::std::move(A.at(k)); }
+
+	size_type row() const { return R; } // the number of rows
+	size_type column() const { return C; }
+
+	BinaryMatrix& operator+=(const BinaryMatrix &B){
+		assert(column() == B.column() && row() == B.row());
+		for(size_type i = 0;i < R;i++)
+			(*this)[i] ^= B[i];
+		return *this;
+	}
+
+	BinaryMatrix& operator-=(const BinaryMatrix &B){
+		assert(column() == B.column() && row() == B.row());
+		for(size_type i = 0;i < R;i++)
+			(*this)[i] ^= B[i];
+		return *this;
+	}
+
+	BinaryMatrix& operator*=(const BinaryMatrix &B){
+		assert(column() == B.row());
+		BinaryMatrix M(R, B.column());
+		for(size_type i = 0;i < R;i++) {
+			for(size_type j = 0;j < B.column();j++) {
+				M[i][j] = 0;
+				for(size_type k = 0;k < C;k++) {
+					M[i][j] ^= ((*this)[i][k] & B[k][j]);
+				}
+			}
+		}
+		swap(M, *this);
+		return *this;
+	}
+
+	void gaussian_elimination() {
+		size_type last_row = 0;
+		for (size_type i = 0; i < C && last_row < R; i++) {
+			for (size_type j = last_row; j < R; j++) {
+				if (A[j][i]) {
+					swap(A[j], A[last_row]);
+					break;
+				}
+			}
+
+			for (size_type j = 0; j < R; j++) {
+				if (last_row == j) continue;
+				if (A[last_row][i] & A[j][i]) {
+					add_row_to_another(j, last_row);
+				}
+			}
+			if (A[last_row][i]) last_row++;
+		}
+	}
+
+	size_type rank() {
+		BinaryMatrix ret = gaussian_elimination();
+		for (size_type i = 0; i < R; i++) {
+			size_type cnt = 0;
+			for (size_type j = 0; j < C; j++) {
+				if (ret[i][j]) cnt++;
+			}
+			if (cnt == 0) return i;
+		}
+		return R;
+	}
+};
